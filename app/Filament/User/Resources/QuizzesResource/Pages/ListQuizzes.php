7696471b267473ac50e108f7fb5ab6900a58411e
pagalworldmp3 <90148815+pagalworldmp3@users.noreply.github.com>
2025-09-26 19:45:38 +0530

    Show upgrade notice on user quiz list when plan limit reached

--- app/Filament/User/Resources/QuizzesResource/Pages/ListQuizzes.php
+++ app/Filament/User/Resources/QuizzesResource/Pages/ListQuizzes.php
@@ -3,8 +3,14 @@
 namespace App\Filament\User\Resources\QuizzesResource\Pages;
 
 use App\Filament\User\Resources\QuizzesResource;
+use App\Enums\SubscriptionStatus;
+use App\Models\Quiz;
+use App\Models\Subscription;
+use Filament\Notifications\Notification;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ListQuizzes extends ListRecords
 {
@@ -12,8 +18,27 @@ class ListQuizzes extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        $actions = [Actions\CreateAction::make()];
+
+        // If user has reached plan limit, show upgrade notice
+        $sub = Subscription::with('plan')
+            ->where('user_id', Auth::id())
+            ->where('status', SubscriptionStatus::ACTIVE->value)
+            ->first();
+        if ($sub && $sub->plan) {
+            $quizCount = Quiz::where('user_id', Auth::id())
+                ->whereBetween('created_at', [$sub->starts_at, $sub->ends_at])
+                ->count();
+            if ($quizCount >= $sub->plan->no_of_quiz) {
+                Notification::make()
+                    ->title(__('Your plan limit is reached. Please upgrade to create more quizzes.'))
+                    ->warning()
+                    ->body(__('Go to Upgrade Plan page to select a higher plan.'))
+                    ->persistent()
+                    ->send();
+            }
+        }
+
+        return $actions;
     }
 }
