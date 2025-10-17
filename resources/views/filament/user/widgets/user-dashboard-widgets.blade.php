a61711d116c950e3ad3093b2ebeb7cb67ac69302
pagalworldmp3 <90148815+pagalworldmp3@users.noreply.github.com>
2025-10-17 11:53:53 +0530

    chore: Rename dashboard quick action labels to Exam terminology
    - 'Create New Quiz' -> 'Create New Exam'
    - 'View All Quizzes' -> 'View All Exams'

--- resources/views/filament/user/widgets/user-dashboard-widgets.blade.php
+++ resources/views/filament/user/widgets/user-dashboard-widgets.blade.php
@@ -19,7 +19,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
                 </svg>
-                Create New Quiz
+                Create New Exam
             </a>
             
             <!-- View Quizzes Button -->
@@ -39,7 +39,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
-                View All Quizzes
+                View All Exams
             </a>
             
             <!-- Manage Subscription Button -->
